71658c8d6021750cb936a4f053c4fdfb4c3542f1
Tare Mautsa <taremautsa@yahoo.com>
2021-10-09 13:05:59 +0200

    added input fields to allow a user to add new shop items to the list of products, created functions to update the list

--- salesTaxCalulator/js/view-model.js
+++ salesTaxCalulator/js/view-model.js
@@ -1,3 +1,29 @@
+function updateShopProducts(newProduct){
+        this.shopItems.push(newProduct);
+        document.getElementById('displayShopItems').innerHTML='';
+        this.displayShopItems();
+}
+
+function clearInputFields(){
+    document.getElementById('itemName').value=null;
+    document.getElementById('itemPrice').value=null;
+    document.getElementById('select').value='Select Category';
+    document.getElementById('import').checked= false;
+}
+
+function addItem(){
+    var itemName= document.getElementById('itemName').value;
+    var itemPrice= document.getElementById('itemPrice').value;
+    var itemCategory= document.getElementById('select').value;
+    var itemImported = document.getElementById('import').checked;
+
+    if(itemImported === true){
+        itemName = 'Imported '+itemName;
+    }
+    this.updateShopProducts({"name": itemName, "price": parseFloat(itemPrice), "isImported": itemImported, "category": itemCategory});
+    this.clearInputFields();
+}
+
 function displayShopItems(){
     let tempShopItems = JSON.parse(JSON.stringify(this.shopItems));
     tempShopItems.forEach((item, i) => {
